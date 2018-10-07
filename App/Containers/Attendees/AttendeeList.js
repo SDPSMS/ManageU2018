8c9ee9a7fa58f1b65b953dd542f148a2ea8e65a2
unmentioned <42333824+unmentioned@users.noreply.github.com>
2018-10-07 19:49:31 +1100

    Added edit and delete button for attendee List

--- App/Containers/Attendees/AttendeeList.js
+++ App/Containers/Attendees/AttendeeList.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
-import { View, Text, FlatList } from 'react-native'
+import { View, Text, FlatList,Button } from 'react-native'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { Colors, Metrics, Fonts } from '../../Themes/'
 import SimpleIcon from 'react-native-vector-icons/SimpleLineIcons'
+import styles from '../Styles/ContainerStyle'
 import _ from 'lodash'
 import 'core-js/es6/symbol'
 import 'core-js/es6/map'
@@ -17,15 +19,26 @@ class AttendeeList extends Component {
           size={30}
           onPress={() => this.props.navigation.pop()}
         />
-        <Text>Attendee Lists</Text>
+        <Text style={styles.sectionText}>List of Attendees</Text>
         <FlatList
           data={this.props.attendeeLists}
           renderItem={
             ({ item }) =>
-              <View>
-                <Text style={{ marginTop: 20 }}>Attendee</Text>
-                <Text>Email: {item.email}</Text>
-                <Text>Name: {item.name}</Text>
+              <View style={{ flexDirection:"row", margin:10, borderBottomWidth:3,borderBottomColor:Colors.cloud}}>
+                <View style={{flex:2, marginLeft:10, marginTop:5}} > 
+                  <Text style={{}}>Attendee</Text>
+                  <Text>Email: {item.email}</Text>
+                  <Text>Name: {item.name}</Text>
+                </View>
+                <View style={{ flex:1,marginRight:10}}>
+                  <View style={{marginBottom:10}}>
+                    <Button title="Edit"  ></Button>
+                  </View>
+                  <View style={{marginBottom:10}}>
+                    <Button title="Delete" color={Colors.fire}></Button>
+                  </View>
+                  
+                </View>
               </View>
           }
           keyExtractor={(item, index) => index.toString()}
